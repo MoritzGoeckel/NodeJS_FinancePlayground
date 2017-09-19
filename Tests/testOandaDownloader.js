1a7b5dd2af0b2e3049a91fb959c71032f178d5f9
Moritz Göckel <moritz.goeckel@gmail.com>
2017-09-19 12:38:49 +0200

    Did the history download

--- Tests/testOandaDownloader.js
+++ Tests/testOandaDownloader.js
@@ -1,3 +1,12 @@
 const dl = require('./../OandaDownload.js');
+const Time = require('./../Time.js')
+const time = new Time();
 
-dl.getInstruments(console.log)
+let to = time.getUTCGetLastEndOfWeekSeconds();
+let from = to - time.daysSeconds(30 * 3);
+
+dl.getInstrument("EUR_USD", "M1", from, to, function(pair, list){
+    console.log(list.length)
+    console.log("Callback!")
+    //console.log(list)
+})
